651984d6f7f7c6fcba04afcfc6ef83422d528fd9
FrInve <francesco1.invernici@mail.polimi.it>
2024-01-19 17:28:20 +0100

    Add parameters and output schema of mineGraphRule

--- src/main/java/it/polimi/deib/rkm/MineGraphRule.java
+++ src/main/java/it/polimi/deib/rkm/MineGraphRule.java
@@ -1,29 +1,37 @@
 package it.polimi.deib.rkm;
 
+import java.util.List;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.Mode;
 import org.neo4j.graphdb.Node;
 
 public class MineGraphRule {
 
-    @Procedure(name = "mineGraphRule")
-    @Description("foo bar")
-    public void mineGraphRule(
+    @Procedure(name = "rkm.mineGraphRule", mode=Mode.READ)
+    @Description("Association Rule Mining for Neo4j")
+    public Stream<AssociationRule> mineGraphRule(
             // Input parameters here
-    ) {
+            @Name("alias")      String alias,
+            @Name("alias_node") String alias_node,
+            @Name("item_head")   List<String> item_head,
+            @Name("item_body")   List<String> item_body,
+            @Name("support")    Number support,
+            @Name("confidence") Number confidence
+            ) {
         // Procedure logic here
-        return;
+        return Stream.of(new AssociationRule(alias));
     }
 
-    public static final class MyRecord {
+    public static final class AssociationRule {
+        public String alias;
 
-        public final Node node;
-
-        MyRecord(Node node) {
-            this.node = node;
+        public AssociationRule(String alias){
+            this.alias = alias;
         }
     }
 }
