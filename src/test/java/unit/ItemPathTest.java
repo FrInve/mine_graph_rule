9cf97cb312e2910c2ee7ee1233ab197126ecff36
FrInve <francesco1.invernici@mail.polimi.it>
2024-01-26 17:56:10 +0100

    Add test classes for ItemPath and Item

---
+++ src/test/java/unit/ItemPathTest.java
@@ -0,0 +1,28 @@
+package unit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ItemPathTest {
+
+    @Test
+    void shouldGenerateMatch(){
+
+        //ItemPath ip = new ItemPath();
+        // String actual ip.toCypherMatch(mode=...)
+        // String expected = ""
+        // String actual ip.toCypherMatch(mode=...)
+        //assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldGenerateReturn(){
+    }
+
+    @Test
+    void shouldGenerateWithReturn(){
+
+    }
+}
