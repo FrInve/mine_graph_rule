b3db1933c4fd3ea324c8ab035446b5d3338da50c
Francesco Cambria <flcambria@gmail.com>
2024-02-01 16:44:08 +0100

    Added test for QueryTest

--- src/main/java/it/polimi/deib/rkm/Query.java
+++ src/main/java/it/polimi/deib/rkm/Query.java
@@ -1,6 +1,7 @@
 package it.polimi.deib.rkm;
 
 import java.util.List;
+import java.util.Map;
 
 public class Query {
     private String alias;
@@ -25,20 +26,36 @@ public class Query {
         this.support = support;
         this.confidence = confidence;
     }
+    public Query(
+            String alias,
+            String aliasNode,
+            Map<String, Object> item_head,
+            Map<String, Object> item_body,
+            double support,
+            double confidence
+    ) {
+        this.alias = alias;
+        this.aliasNode = aliasNode;
+        this.head = new ItemSet(item_head, ItemPath.ItemType.HEAD);
+        this.body = new ItemSet(item_body, ItemPath.ItemType.BODY);
+        this.support = support;
+        this.confidence = confidence;
+    }
+
     public String toCypherForBody() {
-        return "MATCH (n: " +
+        return "MATCH (n:" +
                 this.aliasNode +
                 ")\nWITH n as alias\n" +
                 this.body.toCypherReturn("");
     }
 
     public String toCypherForRule() {
-        return "MATCH (n: " +
+        return "MATCH (n:" +
                 this.aliasNode +
                 ")\nWITH n as alias\n" +
                 this.head.toCypherWith("") +
                 "\n"+
-                this.body.toCypherReturn(this.body.getAliasString());
+                this.body.toCypherReturn(this.head.getAliasString());
     }
 
 }
--- src/test/java/unit/QueryTest.java
+++ src/test/java/unit/QueryTest.java
@@ -4,6 +4,8 @@ import it.polimi.deib.rkm.Query;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -13,32 +15,79 @@ public class QueryTest {
 
     @Test
     void shouldGenerateBody(){
-//        static final Map<Integer, String> MY_MAP = Map.of(
-//                1, "one",
-//                2, "two"
-//        );
+        ArrayList<HashMap<String, String>> itemList = new ArrayList<>();
+        HashMap<String, String> itemMap = new HashMap<>();
+        itemMap.put("type", "normal");
+        itemMap.put("rel_type", "BUY");
+        itemMap.put("rel_alias", "buy");
+        itemMap.put("end_node", "Book");
+        itemMap.put("end_node_alias", "b");
+        itemList.add(itemMap);
+        itemMap = new HashMap<>();
+        itemMap.put("type", "normal");
+        itemMap.put("rel_type", "OF");
+        itemMap.put("rel_alias", "of");
+        itemMap.put("end_node", "Genre");
+        itemMap.put("end_node_alias", "g");
+        itemList.add(itemMap);
+        HashMap<String, Object> itemSetMapBody = new HashMap<>();
+        itemSetMapBody.put("num_min", 1);
+        itemSetMapBody.put("num_max", 1);
+        itemSetMapBody.put("item_path", itemList);
+        HashMap<String, Object> itemSetMapHead = new HashMap<>();
+        itemSetMapHead.put("num_min", 1);
+        itemSetMapHead.put("num_max", 1);
+        itemSetMapHead.put("item_path", itemList);
 
+        Query query = new Query("P",
+                "Person",
+                itemSetMapHead,
+                itemSetMapBody,
+                0.5,
+                0.5);
 
-//        Query query = new Query("P",
-//                "Person",
-//                {Map.of()});
-//
-//        itemMap.put("type", "normal");
-//        itemMap.put("rel_type", "BUY");
-//        itemMap.put("rel_alias", "buy");
-//        itemMap.put("end_node", "Book");
-//        itemMap.put("end_node_alias", "b");
-
-        String actual = "";
-        String expected = "";
+        String actual = query.toCypherForBody();
+        String expected = "MATCH (n:Person)\nWITH n as alias\nMATCH (alias)-[buy:BUY]-(b:Book)-[of:OF]-(g:Genre)\nRETURN len(collect(alias)) as suppcount, b.id as body_BUY_Book, g.id as body_OF_Genre";
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void shouldGenerateRule(){
+        ArrayList<HashMap<String, String>> itemListBody = new ArrayList<>();
+        ArrayList<HashMap<String, String>> itemListHead = new ArrayList<>();
+        HashMap<String, String> itemMap = new HashMap<>();
+        itemMap.put("type", "normal");
+        itemMap.put("rel_type", "BUY");
+        itemMap.put("rel_alias", "buy");
+        itemMap.put("end_node", "Book");
+        itemMap.put("end_node_alias", "b");
+        itemListBody.add(itemMap);
+        itemListHead.add(itemMap);
+        itemMap = new HashMap<>();
+        itemMap.put("type", "normal");
+        itemMap.put("rel_type", "OF");
+        itemMap.put("rel_alias", "of");
+        itemMap.put("end_node", "Genre");
+        itemMap.put("end_node_alias", "g");
+        itemListBody.add(itemMap);
+        HashMap<String, Object> itemSetMapBody = new HashMap<>();
+        itemSetMapBody.put("num_min", 1);
+        itemSetMapBody.put("num_max", 1);
+        itemSetMapBody.put("item_path", itemListBody);
+        HashMap<String, Object> itemSetMapHead = new HashMap<>();
+        itemSetMapHead.put("num_min", 1);
+        itemSetMapHead.put("num_max", 1);
+        itemSetMapHead.put("item_path", itemListHead);
 
-        String actual = "";
-        String expected = "";
+        Query query = new Query("P",
+                "Person",
+                itemSetMapHead,
+                itemSetMapBody,
+                0.5,
+                0.5);
+
+        String actual = query.toCypherForRule();
+        String expected = "MATCH (n:Person)\nWITH n as alias\nMATCH (alias)-[buy:BUY]-(b:Book)\nWITH alias, b.id as head_BUY_Book\nMATCH (alias)-[buy:BUY]-(b:Book)-[of:OF]-(g:Genre)\nRETURN len(collect(alias)) as suppcount, head_BUY_Book, b.id as body_BUY_Book, g.id as body_OF_Genre";
         assertThat(actual).isEqualTo(expected);
 
     }
