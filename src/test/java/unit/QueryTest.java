e9aea5946f1e1355ab62a9e977cd16afeeae951b
FrInve <francesco1.invernici@mail.polimi.it>
2024-02-21 17:06:05 +0100

    Refactor QueryTest to use immutable collections

--- src/test/java/unit/QueryTest.java
+++ src/test/java/unit/QueryTest.java
@@ -4,8 +4,6 @@ import it.polimi.deib.rkm.Query;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,91 +14,98 @@ public class QueryTest {
 
     @Test
     void shouldGenerateBody(){
-        List<Map<String, Object>> itemList = new ArrayList<>();
-        HashMap<String, Object> itemMap = new HashMap<>();
-        itemMap.put("type", "normal");
-        itemMap.put("rel_type", "BUY");
-        itemMap.put("rel_alias", "buy");
-        itemMap.put("end_node", "Book");
-        itemMap.put("end_node_alias", "b");
-        itemList.add(itemMap);
-        itemMap = new HashMap<>();
-        itemMap.put("type", "normal");
-        itemMap.put("rel_type", "OF");
-        itemMap.put("rel_alias", "of");
-        itemMap.put("end_node", "Genre");
-        itemMap.put("end_node_alias", "g");
-        itemList.add(itemMap);
-        HashMap<String, Object> itemSetMapBody = new HashMap<>();
-        itemSetMapBody.put("num_min", 1L);
-        itemSetMapBody.put("num_max", 1L);
-        itemSetMapBody.put("item_path", itemList);
-        HashMap<String, Object> itemSetMapHead = new HashMap<>();
-        itemSetMapHead.put("num_min", 1L);
-        itemSetMapHead.put("num_max", 1L);
-        itemSetMapHead.put("item_path", itemList);
+        List<Map<String,String>> itemPathBodySerialized = List.of(      // Set path of body
+                Map.of("type", "normal",
+                        "rel_type", "BUY",
+                        "rel_alias", "buy",
+                        "end_node", "Book",
+                        "end_node_alias", "b"),
+                Map.of("type", "normal",
+                        "rel_type", "OF",
+                        "rel_alias", "of",
+                        "end_node", "Genre",
+                        "end_node_alias", "g"));
 
-        List<Map<String, Object>> itemSetListBody = new ArrayList<>();
-        List<Map<String, Object>> itemSetListHead = new ArrayList<>();
+        List<Map<String, Object>> itemSetBodySerialized = List.of(      // Set body (only one item)
+                Map.of("num_min", 1L,
+                        "num_max", 1L,
+                        "item_path", itemPathBodySerialized));
 
-        itemSetListBody.add(itemSetMapBody);
-        itemSetListHead.add(itemSetMapHead);
+        List<Map<String,String>> itemPathHeadSerialized = List.of(      // Set path of head
+                Map.of("type", "normal",
+                        "rel_type", "BUY",
+                        "rel_alias", "buy",
+                        "end_node", "Book",
+                        "end_node_alias", "b"));
+
+        List<Map<String, Object>> itemSetHeadSerialized = List.of(      // Set head (only one item)
+                Map.of("num_min", 1L,
+                        "num_max", 1L,
+                        "item_path", itemPathHeadSerialized));
 
         Query query = new Query("P",
                 "Person",
-                itemSetListHead,
-                itemSetListBody,
+                itemSetHeadSerialized,
+                itemSetBodySerialized,
                 0.5,
                 0.5);
 
         String actual = query.toCypherForBody();
-        String expected = "MATCH (n:Person)\nWITH n as alias\nMATCH (alias)-[buy:BUY]-(b:Book)-[of:OF]-(g:Genre)\nRETURN size(collect(DISTINCT alias)) as suppcount, b.id as body_BUY_Book, g.id as body_OF_Genre";
+        String expected = """
+        MATCH (n:Person)
+        WITH n as alias
+        MATCH (alias)-[buy:BUY]-(b:Book)-[of:OF]-(g:Genre)
+        RETURN size(collect(DISTINCT alias)) as suppcount, b.id as body_BUY_Book, g.id as body_OF_Genre""";
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void shouldGenerateRule(){
-        List<Map<String, Object>> itemListBody = new ArrayList<>();
-        List<Map<String, Object>> itemListHead = new ArrayList<>();
-        HashMap<String, Object> itemMap = new HashMap<>();
-        itemMap.put("type", "normal");
-        itemMap.put("rel_type", "BUY");
-        itemMap.put("rel_alias", "buy");
-        itemMap.put("end_node", "Book");
-        itemMap.put("end_node_alias", "b");
-        itemListBody.add(itemMap);
-        itemListHead.add(itemMap);
-        itemMap = new HashMap<>();
-        itemMap.put("type", "normal");
-        itemMap.put("rel_type", "OF");
-        itemMap.put("rel_alias", "of");
-        itemMap.put("end_node", "Genre");
-        itemMap.put("end_node_alias", "g");
-        itemListBody.add(itemMap);
-        HashMap<String, Object> itemSetMapBody = new HashMap<>();
-        itemSetMapBody.put("num_min", 1L);
-        itemSetMapBody.put("num_max", 1L);
-        itemSetMapBody.put("item_path", itemListBody);
-        HashMap<String, Object> itemSetMapHead = new HashMap<>();
-        itemSetMapHead.put("num_min", 1L);
-        itemSetMapHead.put("num_max", 1L);
-        itemSetMapHead.put("item_path", itemListHead);
+        List<Map<String,String>> itemPathBodySerialized = List.of(      // Set path of body
+                Map.of("type", "normal",
+                        "rel_type", "BUY",
+                        "rel_alias", "buy",
+                        "end_node", "Book",
+                        "end_node_alias", "b"),
+                Map.of("type", "normal",
+                        "rel_type", "OF",
+                        "rel_alias", "of",
+                        "end_node", "Genre",
+                        "end_node_alias", "g"));
 
-        List<Map<String, Object>> itemSetListBody = new ArrayList<>();
-        List<Map<String, Object>> itemSetListHead = new ArrayList<>();
+        List<Map<String, Object>> itemSetBodySerialized = List.of(      // Set body (only one item)
+                Map.of("num_min", 1L,
+                        "num_max", 1L,
+                        "item_path", itemPathBodySerialized)
+        );
 
-        itemSetListBody.add(itemSetMapBody);
-        itemSetListHead.add(itemSetMapHead);
+        List<Map<String,String>> itemPathHeadSerialized = List.of(      // Set path of head
+                Map.of("type", "normal",
+                        "rel_type", "BUY",
+                        "rel_alias", "buy",
+                        "end_node", "Book",
+                        "end_node_alias", "b"));
+
+        List<Map<String, Object>> itemSetHeadSerialized = List.of(      // Set head (only one item)
+                Map.of("num_min", 1L,
+                        "num_max", 1L,
+                        "item_path", itemPathHeadSerialized));
+
         Query query = new Query("P",
                 "Person",
-                itemSetListHead,
-                itemSetListBody,
+                itemSetHeadSerialized,
+                itemSetBodySerialized,
                 0.5,
                 0.5);
 
         String actual = query.toCypherForRule();
-        String expected = "MATCH (n:Person)\nWITH n as alias\nMATCH (alias)-[buy:BUY]-(b:Book)\nWITH alias, b.id as head_BUY_Book\nMATCH (alias)-[buy:BUY]-(b:Book)-[of:OF]-(g:Genre)\nRETURN size(collect(DISTINCT alias)) as suppcount, head_BUY_Book, b.id as body_BUY_Book, g.id as body_OF_Genre";
+        String expected = """
+        MATCH (n:Person)
+        WITH n as alias
+        MATCH (alias)-[buy:BUY]-(b:Book)
+        WITH alias, b.id as head_BUY_Book
+        MATCH (alias)-[buy:BUY]-(b:Book)-[of:OF]-(g:Genre)
+        RETURN size(collect(DISTINCT alias)) as suppcount, head_BUY_Book, b.id as body_BUY_Book, g.id as body_OF_Genre""";
         assertThat(actual).isEqualTo(expected);
-
     }
 }
